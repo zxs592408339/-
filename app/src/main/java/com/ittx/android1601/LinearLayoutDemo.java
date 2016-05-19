bad30fbf1207ece2c0d948e6dd23023b7692093a
viktor.zhou <2915858910@qq.com>
2016-05-19 17:32:25 +0800

    Initial commit

---
+++ app/src/main/java/com/ittx/android1601/LinearLayoutDemo.java
@@ -0,0 +1,12 @@
+package com.ittx.android1601;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class LinearLayoutDemo extends AppCompatActivity{
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        this.setContentView(R.layout.activity_relative_layout);
+    }
+}
---
+++ app/src/main/java/com/ittx/android1601/MainActivity.java
@@ -0,0 +1,13 @@
+package com.ittx.android1601;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main); //将activity_main布局文件加载到activity界面上
+    }
+}
